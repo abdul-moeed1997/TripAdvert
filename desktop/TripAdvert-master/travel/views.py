c7c26d286b77d9da5c40c5e85d81d6c7ab2c8e66
mukarram786 <mukarram8888@gmail.com>
2019-12-08 00:01:01 +0500

    loginSignUpProfile

---
+++ desktop/TripAdvert-master/travel/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+def signUp(request):
+    return render(request,'register.html')
+def profile(request):
+    return render(request,'profile.html')
