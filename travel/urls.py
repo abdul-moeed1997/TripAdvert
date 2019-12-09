9f688ee1fa1cd020165a46a4911978e869ae683a
mukarram786 <mukarram8888@gmail.com>
2019-12-09 14:44:36 +0500

    template convertion by mukarram

--- travel/urls.py
+++ travel/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='Home'),
     path('tours/', views.tours, name='Tours'),
     path('tour-detail/', views.tourDetail, name='Tour-Details'),
+    path('login', views.login, name='Login'),
+    path('signUp', views.signUp, name='signup'),
+    path('profile', views.profile , name='profile')
 ]
