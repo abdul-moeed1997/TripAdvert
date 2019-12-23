7caabf7ac16b9851213ff9287badc5fd13c37011
Abdul Moeed <moeedsheikh8@gmail.com>
2019-12-23 14:44:06 +0500

    adding Edit Profile template and removing some previous errors

--- travel/views.py
+++ travel/views.py
@@ -6,23 +6,36 @@ def index(request):
 
 def tours(request):
     return render(request,'all-package.html')
+
 def tourDetail(request):
     return render(request,'tour-details.html')
+
 def login(request):
     return render(request,'login.html')
+
 def signUp(request):
     return render(request,'register.html')
-def profile(request):
-    return render(request,'profile.html')
+
+def myProfile(request):
+    return render(request,'my-profile.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
 def eventBooking(request):
     return render(request,'eventBooking.html')
+
 def editProfile(request):
     return render(request,'edit-Profile.html')
+
 def contact(request):
     return render(request,'contact.html')
+
 def not_found(request):
     return render(request, '404.html')
+
 def about(request):
     return render(request, 'about.html')
+
 def price_list(request):
     return render(request, 'price-list.html')
